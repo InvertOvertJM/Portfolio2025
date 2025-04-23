d9a7557e74dfd4185f783ab4396179e1bba2d0c3
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-04-23 11:25:41 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
+$(document).ready(function() {
+
+
 const stickmanImg = document.getElementById("stickman-img");
 const stickman = document.querySelector(".stickman");
-const ores = document.querySelectorAll(".ore");
+
 
 let scrollTimer;
 let isClimbing = false;
 
 
-window.addEventListener("scroll", () => {
+document.addEventListener("scroll", () => {
   const scrollY = window.scrollY;
-  stickman.style.top = `${scrollY * 1.2}px`; //multiplier for speed
+  stickman.style.top = `${scrollY * 1.6}px`; //multiplier for speed
 })
 
-// Event listener for ore clicks
-ores.forEach(ore => {
-  ore.addEventListener("click", () => {
-    const projectLabel = ore.nextElementSibling.textContent; // Get the label text
-    alert(`You clicked on the project: ${projectLabel}`); // Example action
-  });
+$("#wcorp").click(function(){
+  $("#wcorp").css("display", "none")
 });
 
-// Event listeners for footer buttons
-document.getElementById("sackBtn").addEventListener("click", () => {
-  alert("You clicked the Sack!"); // Replace with your own action
-});
-
-document.getElementById("phoneBtn").addEventListener("click", () => {
-  alert("You clicked the Phone!"); // Replace with your own action
 });
