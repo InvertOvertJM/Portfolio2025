dcc09acd09a112df685a6a6b272fcea364dcfe95
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-04-23 11:44:19 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,14 +8,41 @@ const stickman = document.querySelector(".stickman");
 let scrollTimer;
 let isClimbing = false;
 
-
-document.addEventListener("scroll", () => {
-  const scrollY = window.scrollY;
-  stickman.style.top = `${scrollY * 1.6}px`; //multiplier for speed
+$(document).scroll(function(){
+const scrollY = window.scrollY;
+  $(".stickman").css("top",`${scrollY * 1.6}px`)
 })
 
 $("#wcorp").click(function(){
-  $("#wcorp").css("display", "none")
+  $("#wcorp").css("display", "none");
+});
+
+$("#clasher").click(function(){
+  $("#clasher").css("display", "none");
+});
+
+$("#news").click(function(){
+  $("#news").css("display", "none");
+});
+
+$("#themed").click(function(){
+  $("#themed").css("display", "none");
+});
+
+$("#clasherv1").click(function(){
+  $("#clasherv1").css("display", "none");
+});
+
+$("#lob").click(function(){
+  $("#lob").css("display", "none");
+});
+
+$("#faith").click(function(){
+  $("#faith").css("display", "none");
+});
+
+$("#scaling").click(function(){
+  $("#scaling").css("display", "none");
 });
 
 });
