ee13adcd5c9d47c8466f5a26867f161d9fd4ba38
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-04-24 11:22:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -25,33 +25,60 @@ $(document).ready(function () {
 
   $("#scaling").click(function () {
     $(this).hide();
+    $("#scalingG").css("display","block");
+    $("#sackBtn").css("color", "red");
   });
 
   $("#faith").click(function () {
     $(this).hide();
+    $("#faithG").css("display","block");
+    $("#sackBtn").css("color", "red");
   });
 
   $("#lob").click(function () {
     $(this).hide();
+    $("#lobG").css("display","block");
+    $("#sackBtn").css("color", "red");
   });
 
   $("#clasherv1").click(function () {
     $(this).hide();
+    $("#clasherv1G").css("display","block");
+    $("#sackBtn").css("color", "red");
   });
 
   $("#themed").click(function () {
     $(this).hide();
+    $("#themedG").css("display","block");
+    $("#sackBtn").css("color", "red");
   });
 
   $("#news").click(function () {
     $(this).hide();
+    $("#newsG").css("display","block");
+    $("#sackBtn").css("color", "red");
   });
 
   $("#clasher").click(function () {
     $(this).hide();
+    $("#clasherv2G").css("display","block");
+    $("#sackBtn").css("color", "red");
   });
 
   $("#wcorp").click(function () {
     $(this).hide();
+    $("#wcorpG").css("display","block");
+    $("#sackBtn").css("color", "red");
+  });
+
+  $("#sackBtn").click(function (){
+    $("#oreGUI").css("display","block");
+    $("#sackBtn").css("color", "white");
+  });
+
+  $("#closeOreGUI").click(function (){
+    $("#oreGUI").css("display","none");
   });
 });
+
+  
