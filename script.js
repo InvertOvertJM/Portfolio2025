040ccc06e4d29b26ccf07b1633343b437c32e9ac
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-04-24 00:22:11 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,89 +1,57 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  const images = [
+    "sequence/1.jfif",
+    "sequence/2.jpeg",
+    "sequence/3.jpeg",
+    "sequence/4.jpeg",
+    "sequence/5.jpeg",
+    "sequence/6.jpeg"
+  ];
 
+  const totalFrames = images.length;
+  const stickmanImg = document.getElementById("stickman-img");
+  const stickman = $(".stickman");
 
-const stickmanImg = document.getElementById("stickman-img");
-const stickman = document.querySelector(".stickman");
+  $(window).on("scroll", function () {
+    const scrollTop = $(window).scrollTop();
+    const maxScroll = $(document).height() - $(window).height();
+    const progress = scrollTop / maxScroll;
+    const frameIndex = Math.floor(progress * totalFrames);
+    const clampedIndex = Math.min(frameIndex, totalFrames - 1);
+    stickmanImg.src = images[clampedIndex];
 
+    stickman.css("top", `${scrollTop * 1.6}px`);
+  });
 
-let scrollTimer;
-let isClimbing = false;
+  $("#scaling").click(function () {
+    $(this).hide();
+  });
 
-$(document).scroll(function(){
-const scrollY = window.scrollY;
-  $(".stickman").css("top",`${scrollY * 1.6}px`)
-})
+  $("#faith").click(function () {
+    $(this).hide();
+  });
 
-// define images
-var images = [
-  "sequence/1.jfif",
-  "sequence/2.jpeg",
-  "sequence/3.jpeg",
-  "sequence/4.jpeg",
-  "sequence/5.jpeg",
-  "sequence/6.jpeg",
-];
+  $("#lob").click(function () {
+    $(this).hide();
+  });
 
-// TweenMax can tween any property of any object. We use this object to cycle through the array
-var obj = {curImg: 0};
+  $("#clasherv1").click(function () {
+    $(this).hide();
+  });
 
-// create tween
-var tween = TweenMax.to(obj, 0.5,
-  {
-    curImg: images.length - 1,	// animate propery curImg to number of images
-    roundProps: "curImg",				// only integers so it can be used as an array index
-    repeat: 3,									// repeat 3 times
-    immediateRender: true,			// load first image automatically
-    ease: Linear.easeNone,			// show every image the same ammount of time
-    onUpdate: function () {
-      $("#stickman-img").attr("src", images[obj.curImg]); // set the image source
-    }
-  }
-);
+  $("#themed").click(function () {
+    $(this).hide();
+  });
 
-// init controller
-var controller = new ScrollMagic.Controller();
+  $("#news").click(function () {
+    $(this).hide();
+  });
 
-// build scene
-var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 300})
-        .setTween(tween)
-        .addIndicators() // add indicators (requires plugin)
-        .addTo(controller);
+  $("#clasher").click(function () {
+    $(this).hide();
+  });
 
-// handle form change
-$("form.move input[name=duration]:radio").change(function () {
-  scene.duration($(this).val());
-});
-
-$("#wcorp").click(function(){
-  $("#wcorp").css("display", "none");
-});
-
-$("#clasher").click(function(){
-  $("#clasher").css("display", "none");
-});
-
-$("#news").click(function(){
-  $("#news").css("display", "none");
-});
-
-$("#themed").click(function(){
-  $("#themed").css("display", "none");
-});
-
-$("#clasherv1").click(function(){
-  $("#clasherv1").css("display", "none");
-});
-
-$("#lob").click(function(){
-  $("#lob").css("display", "none");
-});
-
-$("#faith").click(function(){
-  $("#faith").css("display", "none");
-});
-
-$("#scaling").click(function(){
-  $("#scaling").css("display", "none");
-});
-
+  $("#wcorp").click(function () {
+    $(this).hide();
+  });
 });
