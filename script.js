06313166be29ae4b2f241200afd1c654da74a302
Overt$!#& <135649678+InvertOvertJM@users.noreply.github.com>
2025-04-25 03:18:52 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   const images = [
-    "sequence/1.jfif",
-    "sequence/2.jpeg",
-    "sequence/3.jpeg",
-    "sequence/4.jpeg",
-    "sequence/5.jpeg",
-    "sequence/6.jpeg"
+    "sequence/1.png",
+    "sequence/2.png",
+    "sequence/3.png",
+    "sequence/4.png",
+    "sequence/5.png",
+    "sequence/6.png"
   ];
 
   const totalFrames = images.length;
@@ -20,7 +20,7 @@ $(document).ready(function () {
     const clampedIndex = Math.min(frameIndex, totalFrames - 1);
     stickmanImg.src = images[clampedIndex];
 
-    stickman.css("top", `${scrollTop * 1.8}px`);
+    stickman.css("top", `${scrollTop * 1.48}px`);
   });
 
   $("#scaling").click(function () {
@@ -83,6 +83,87 @@ $(document).ready(function () {
   $("#phoneBtn").click(function () {
     alert("PHONE")
   });
+
+  const oreData = {
+    scalingG: {
+      title: "Scaling Website",
+      type: "Common Rarity Ore due to its simplicity and reused assets",
+      desc: "A website exploring how video games handle difficulty scaling",
+      link: "https://github.com/InvertOvertJM/Scaling-Website"
+    },
+    faithG: {
+      title: "Faith App",
+      type: "Common Rarity Ore due to it being one of the first projects on code.org",
+      desc: "A small app advertising FAITH THE UNHOLY TRINITY",
+      link: "https://studio.code.org/projects/applab/WBNUWYVUOTqVPWGhYns1KB7VQJBt119zM21kZfNtcb8"
+    },
+    lobG: {
+      title: "LobCorp",
+      type: "Common Rarity Ore due to it being very simplistic, as well as it being one of the first times I utilized Bootstrap",
+      desc: "A fan project based on the Project Moon Games, depicting Lobotomy Corporation as a Zoo",
+      link: "https://l-corp-zoo-project-6812042.codehs.me/index.html"
+    },
+    clasherv1G: {
+      title: "Clasher V1",
+      type: "Silver Rarity Ore for it being a sort of complicated game",
+      desc: "First Version of the Clasher game, Inspired by Project Moon Games, Developed for the APCSP Exam",
+      link: "https://studio.code.org/projects/applab/HeJHV5_2I8itP0DUI1gjT8nHrrftOcrasV8X5R2tl0I"
+    },
+    themedG: {
+      title: "Themed Website",
+      type: "Silver Rarity Ore, first time utilizing JS Canvas'. Personally don't like it very much",
+      desc: "A Halloween site based on Limbus Company",
+      link: "https://github.com/InvertOvertJM/Themed-Website"
+    },
+    newsG: {
+      title: "Newsletter",
+      type: "Diamond Rarity Ore, very clean look that I'm proud of, as well as a project I took charge of.",
+      desc: "A Newsletter webpage prototype built for ITHS",
+      link: "https://github.com/InvertOvertJM/Themed-Website"
+    },
+    clasherv2G: {
+      title: "Clasher V2",
+      type: "Diamond Rarity Ore, love the way this turned out, second iteration of Clasher.",
+      desc: "A turn based battler, Inspired by Project Moon Games",
+      link: "https://github.com/InvertOvertJM/Clasher"
+    },
+    wcorpG: {
+      title: "W Corp",
+      type: "Diamond Rarity Ore, another website with a very clean design, first time utilizing JQueryUI for elements within a website",
+      desc: "An exploration into the world of W Corp.",
+      link: "https://github.com/InvertOvertJM/jQueryUI"
+    }
+  };
+  
+  $(".ore-shape").on("click", function () {
+    const id = $(this).attr("id");
+    const data = oreData[id];
+  
+    if (data) {
+      $("#oreTitle").text(data.title);
+      $("#oreType").text(data.type);
+      $("#oreDescription").text(data.desc);
+      $("#oreLink").attr("href", data.link);
+      gsap.to("#oreDetailGUI", { bottom: 0, duration: 0.4 });
+    }
+  });
+  
+  $("#closeOreDetailGUI").on("click", function () {
+    gsap.to("#oreDetailGUI", { bottom: "-100%", duration: 0.4 });
+  });
+  
+  
+  $(document).mousemove(function(event) {
+    const x = event.clientX;
+    const y = event.clientY;
+  
+    $('#flashlight').css({
+      'left': x - $('#flashlight').width() / 2,
+      'top': y - $('#flashlight').height() / 2
+    });
+  });
+  
+
 });
 
   
